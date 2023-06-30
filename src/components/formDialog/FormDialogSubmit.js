e94c1a768a2e4bbd53abdfac1780cfff4b2aa5f9
tanan2005 <thanhan18072000@gmail.com>
2023-06-30 16:42:39 +0700

    Update new

---
+++ src/components/formDialog/FormDialogSubmit.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function FormDialogSubmit({
+  children,
+  open,
+  title,
+  handleClickOpen,
+  handleSubmit,
+  handleClose,
+  ok,
+  close,
+}) {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>{children}</DialogContent>
+      </Dialog>
+    </div>
+  );
+}
